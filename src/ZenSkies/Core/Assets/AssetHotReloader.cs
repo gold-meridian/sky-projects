c686f7ea3b254197c23f62bd3cc9c79852500cf5
Zoe <88523932+ZenTheMod@users.noreply.github.com>
2025-12-26 18:40:33 -0800

    add asset reloader proper

---
+++ src/ZenSkies/Core/Assets/AssetHotReloader.cs
@@ -0,0 +1,189 @@
+﻿using Daybreak.Common.Features.Hooks;
+using ReLogic.Content;
+using ReLogic.Content.Sources;
+using ReLogic.Utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ZenSkies.Core.Assets;
+
+#if DEBUG
+
+// TODO: Prevent image premultiplication.
+[Autoload(Side = ModSide.Client)]
+internal static class AssetReloader
+{
+    private const NotifyFilters all_filters =
+        NotifyFilters.FileName |
+        NotifyFilters.DirectoryName |
+        NotifyFilters.Attributes |
+        NotifyFilters.Size |
+        NotifyFilters.LastWrite |
+        NotifyFilters.LastAccess |
+        NotifyFilters.CreationTime |
+        NotifyFilters.Security;
+
+    private static FileSystemWatcher? assetWatcher;
+
+    private static string modSource = string.Empty;
+
+    private static LocalAssetSource? assetSource;
+
+    private readonly static Mod mod = ModContent.GetInstance<ModImpl>();
+
+    [OnLoad]
+    private static void Load()
+    {
+        try
+        {
+            modSource = mod.SourceFolder.Replace('\\', '/');
+
+            if (!Directory.Exists(modSource))
+            {
+                throw new DirectoryNotFoundException("Mod source directory does not exsist!");
+            }
+
+            assetSource = new LocalAssetSource(modSource);
+
+            ChangeContentSource();
+
+            AssetReaderCollection assetReaderCollection = Main.instance.Services.Get<AssetReaderCollection>();
+
+            string[] extensions = assetReaderCollection.GetSupportedExtensions();
+
+            assetWatcher = new(modSource);
+
+            foreach (string e in extensions)
+            {
+                assetWatcher.Filters.Add($"*{e}");
+            }
+
+            assetWatcher.Changed += AssetChanged;
+
+            assetWatcher.NotifyFilter = all_filters;
+
+            assetWatcher.IncludeSubdirectories = true;
+            assetWatcher.EnableRaisingEvents = true;
+        }
+        catch (Exception e)
+        {
+            mod.Logger.Warn($"Unable to load Asset Reloader! - {e}");
+        }
+    }
+
+    [OnUnload]
+    private static void Unload()
+    {
+        if (assetWatcher is not null)
+        {
+            assetWatcher.EnableRaisingEvents = false;
+            assetWatcher.Dispose();
+        }
+    }
+
+    private static void AssetChanged(object sender, FileSystemEventArgs e)
+    {
+        if (e.ChangeType.HasFlag(WatcherChangeTypes.Created))
+        {
+            return;
+        }
+
+        string assetPath = Path.GetRelativePath(modSource, e.FullPath).Replace('/', '\\');
+
+        assetSource?.AddAssetPath(assetPath);
+
+        assetPath = Path.ChangeExtension(assetPath, null);
+
+        if (e.ChangeType.HasFlag(WatcherChangeTypes.Deleted) ||
+            e.ChangeType.HasFlag(WatcherChangeTypes.Renamed))
+        {
+            mod.Logger.Warn($"Asset at {assetPath} was removed or renamed!");
+
+            return;
+        }
+
+        var repositoryAssets = mod.Assets._assets;
+
+        Debug.Assert(repositoryAssets is not null);
+
+        if (!repositoryAssets.TryGetValue(assetPath, out IAsset? asset) ||
+            asset is null)
+        {
+            return;
+        }
+
+        Main.QueueMainThreadAction(() => ReloadAsset(asset));
+    }
+
+    private static void ReloadAsset(IAsset asset)
+    {
+        lock (mod.Assets._requestLock)
+        {
+            mod.Assets.ForceReloadAsset(asset, AssetRequestMode.ImmediateLoad);
+        }
+
+        InvokeAssetWait(asset);
+    }
+
+    private static void InvokeAssetWait(IAsset asset)
+    {
+        Type type = asset.GetType();
+
+        if (!type.IsGenericType ||
+            type.GetGenericTypeDefinition() != typeof(Asset<>))
+        {
+            throw new ArgumentException($"Asset was of incorrect type!");
+        }
+
+        MethodInfo? getAssetWait = type.GetProperty(nameof(Asset<>.Wait), BindingFlags.Public | BindingFlags.Instance)?.GetGetMethod();
+
+        var wait = (Action?)getAssetWait?.Invoke(asset, []);
+
+        wait?.Invoke();
+    }
+
+    private static void ChangeContentSource()
+    {
+        Main.QueueMainThreadAction(
+            () =>
+            {
+                mod.Assets.SetSources([assetSource, mod.RootContentSource]);
+            }
+        );
+    }
+
+    internal sealed class LocalAssetSource : ContentSource
+    {
+        private string ModSource { get; init; }
+
+        public string[] AssetPaths
+        {
+            get => assetPaths;
+            set => SetAssetNames(value);
+        }
+
+        public LocalAssetSource(string modSource) : base()
+        {
+            ModSource = modSource;
+
+            assetPaths = [];
+        }
+
+        public override Stream OpenStream(string fullAssetName)
+        {
+            return File.OpenRead(Path.Combine(ModSource, fullAssetName));
+        }
+
+        public void AddAssetPath(string path)
+        {
+            AssetPaths = AssetPaths.Append(path).ToArray();
+        }
+    }
+}
+
+#endif
---
+++ src/ZenSkies/Core/Assets/ShaderHotCompiler.cs
@@ -0,0 +1,229 @@
+﻿using Daybreak.Common.Features.Hooks;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using static System.IO.WatcherChangeTypes;
+
+namespace ZenSkies.Core.Assets;
+
+#if DEBUG
+
+[Autoload(Side = ModSide.Client)]
+public static class ShaderHotCompiler
+{
+    private const NotifyFilters all_filters =
+        NotifyFilters.FileName |
+        NotifyFilters.DirectoryName |
+        NotifyFilters.Attributes |
+        NotifyFilters.Size |
+        NotifyFilters.LastWrite |
+        NotifyFilters.LastAccess |
+        NotifyFilters.CreationTime |
+        NotifyFilters.Security;
+
+    private static readonly string[] effect_extensions = [".fx", ".hlsl"];
+
+    private static string fxcPath = "";
+
+    private static FileSystemWatcher? effectWatcher;
+
+    private static string modSource = "";
+
+    private readonly static Mod mod = ModContent.GetInstance<ModImpl>();
+
+
+    [OnLoad]
+    private static void Load()
+    {
+        try
+        {
+            modSource = mod.SourceFolder.Replace('\\', '/');
+
+            string[] paths = Directory.GetFiles(modSource, "*fxc.exe", SearchOption.AllDirectories);
+
+            if (paths.Length <= 0)
+            {
+                mod.Logger.Info("'fxc.exe' not found! Effects will not be compiled!");
+                return;
+            }
+
+            fxcPath = paths[0].Replace('\\', '/');
+
+            effectWatcher = new(modSource);
+
+            foreach (string e in effect_extensions)
+                effectWatcher.Filters.Add($"*{e}");
+
+            effectWatcher.Changed += EffectChanged;
+
+            effectWatcher.NotifyFilter = all_filters;
+
+            effectWatcher.IncludeSubdirectories = true;
+            effectWatcher.EnableRaisingEvents = true;
+        }
+        catch (Exception e)
+        {
+            mod.Logger.Warn($"Unable to load Shader Hot-Compiler! - {e}");
+        }
+    }
+
+    [OnUnload]
+    private static void Unload()
+    {
+        effectWatcher?.Dispose();
+    }
+
+    private static void EffectChanged(object sender, FileSystemEventArgs e)
+    {
+        if (e.ChangeType.HasFlag(Created))
+            return;
+
+        string effectPath = e.FullPath.Replace('\\', '/');
+
+        string shortPath = Path.GetRelativePath(modSource, effectPath);
+
+        shortPath = Path.ChangeExtension(shortPath, null).Replace('\\', '/');
+
+        if (e.ChangeType.HasFlag(Deleted) ||
+            e.ChangeType.HasFlag(Renamed))
+        {
+            mod.Logger.Warn($"Effect at {shortPath} was removed or renamed!");
+            return;
+        }
+
+        Task.Run(
+            async () =>
+            {
+                await CompileShaderTask(fxcPath, effectPath, shortPath);
+            }
+        );
+    }
+
+    private static async Task CompileShaderTask(string executable, string effectPath, string shortPath)
+    {
+        // Prevent alledged issues with temp files.
+        await Task.Delay(10);
+
+        string wineArgument = "";
+
+        string outputEffect = Path.ChangeExtension(effectPath, ".fxc");
+
+        if (OperatingSystem.IsLinux())
+        {
+            HandleWineCompilation(ref executable, ref wineArgument, ref effectPath, ref outputEffect);
+        }
+
+        ProcessStartInfo pInfo = new()
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = executable,
+            Arguments = $"{wineArgument} \"{effectPath}\" /T fx_2_0 /nologo /O2 /Fo \"{outputEffect}\"",
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
+
+        using Process process = new();
+
+        process.StartInfo = pInfo;
+
+        process.ErrorDataReceived +=
+            (_, e) =>
+            {
+                LogShaderCompilationError(e.Data ?? string.Empty, effectPath, shortPath);
+            };
+
+        process.Start();
+
+        process.BeginErrorReadLine();
+
+        process.WaitForExit();
+
+        if (process.ExitCode == 0)
+            return;
+
+        mod.Logger.Warn($"Effect at {shortPath} could not be compiled! Exit code: {process.ExitCode}");
+    }
+
+    private static void HandleWineCompilation(ref string executable, ref string wineArgument, ref string effectPath, ref string outputEffect)
+    {
+        ProcessStartInfo pInfo = new()
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "/bin/bash",
+            Arguments = "-c \"command -v wine\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
+
+        using Process process = new();
+
+        process.StartInfo = pInfo;
+        process.Start();
+
+        process.WaitForExit();
+
+        string error = process.StandardError.ReadToEnd();
+        string output = process.StandardOutput.ReadToEnd();
+
+        Debug.Assert(!string.IsNullOrEmpty(error));
+        Debug.Assert(!string.IsNullOrEmpty(output));
+
+        wineArgument = executable;
+        executable = output.Trim();
+
+        WinePathConversion(ref effectPath);
+        WinePathConversion(ref outputEffect);
+    }
+
+    private static void WinePathConversion(ref string path)
+    {
+        ProcessStartInfo pInfo = new()
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "/bin/bash",
+            Arguments = $"-c \"winepath --windows '{path}'\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
+
+        using Process process = new();
+
+        process.StartInfo = pInfo;
+        process.Start();
+
+        process.WaitForExit();
+
+        string error = process.StandardError.ReadToEnd();
+        string output = process.StandardOutput.ReadToEnd();
+
+        if (string.IsNullOrEmpty(output))
+        {
+            mod.Logger.Warn($"Error converting path \"{path}\" using WINE; {error}");
+        }
+
+        path = output.Trim();
+    }
+
+    private static void LogShaderCompilationError(string error, string effectPath, string shortPath)
+    {
+        if (error.Length <= 0)
+        {
+            return;
+        }
+
+        error = error.Replace(effectPath, string.Empty);
+
+        if (!error.Contains("error"))
+        {
+            return;
+        }
+
+        mod.Logger.Warn($"{shortPath}: {error}");
+    }
+}
+
+#endif
